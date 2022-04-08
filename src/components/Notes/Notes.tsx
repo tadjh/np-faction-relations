0571ef727742be75b41e6724168260a015e484cc
TadjhBrooks <tadjh.brooks@gmail.com>
2022-04-08 12:31:34 -0700

    Resized and disabled on mount

--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -7,14 +7,14 @@ import {
   useRef,
   useState,
 } from 'react';
-import CloseIcon from '../../assets/Icons';
+import { CloseIcon } from '../../assets/Icons';
 import { LABEL_TEXT_NOTES } from '../../config/strings';
 import text from './text';
 
-const duration = 15000;
-const scalar = 10000 / duration;
+const FIFTEEN_SECONDS_IN_MILLISECONDS = 15000;
+const scalar = 10000 / FIFTEEN_SECONDS_IN_MILLISECONDS;
 
-const transitionDuration = 150;
+const TRANSITION_DURATION_IN_MILLISECONDS = 150;
 
 const variants = {
   enter: {
@@ -34,15 +34,15 @@ const variants = {
 };
 
 function Notes() {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false);
   const [slide, setSlide] = useState(0);
-  const [isPaused, setIsPaused] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
   const [progress, setProgress] = useState(0);
   const requestRef = useRef<number | null>(null);
   const previousTimeRef = useRef<number | null>(null);
 
   const animate = useCallback((time: number) => {
-    if (previousTimeRef.current !== null) {
+    if (previousTimeRef.current) {
       const deltaTime = time - previousTimeRef.current;
       setProgress(
         (prevProgress) => (prevProgress + deltaTime * 0.01 * scalar) % 100
@@ -52,51 +52,84 @@ function Notes() {
     requestRef.current = requestAnimationFrame(animate);
   }, []);
 
+  const cancel = (handle: number | null) => {
+    if (handle === null) return;
+    cancelAnimationFrame(handle);
+  };
+
   useEffect(() => {
+    if (!isVisible) return;
     requestRef.current = requestAnimationFrame(animate);
-    return () => {
-      requestRef.current !== null && cancelAnimationFrame(requestRef.current);
-    };
-  }, [animate]);
+    return () => cancel(requestRef.current);
+  }, [isVisible, animate]);
+
+  const increment = useCallback(() => {
+    slide === text.length - 1
+      ? setSlide(0)
+      : setSlide((prevSlide) => prevSlide + 1);
+  }, [slide]);
+
+  const decrement = useCallback(() => {
+    slide === 0
+      ? setSlide(text.length - 1)
+      : setSlide((prevSlide) => prevSlide - 1);
+  }, [slide]);
 
   useEffect(() => {
     const interval: NodeJS.Timer = setInterval(() => {
       if (isPaused) return clearInterval(interval);
+
       const timeout: NodeJS.Timer = setTimeout(() => {
         setProgress(0);
-        slide === text.length - 1
-          ? setSlide(0)
-          : setSlide((prevSlide) => prevSlide + 1);
+        increment();
         requestRef.current = requestAnimationFrame(animate);
         return clearTimeout(timeout);
-      }, transitionDuration);
-    }, duration);
+      }, TRANSITION_DURATION_IN_MILLISECONDS);
+    }, FIFTEEN_SECONDS_IN_MILLISECONDS);
 
     return () => clearInterval(interval);
-  }, [slide, isPaused, animate]);
+  }, [isPaused, animate, increment]);
 
   useEffect(() => {
     if (Math.round(progress) === 100)
       return () => {
         previousTimeRef.current = null;
-        requestRef.current !== null && cancelAnimationFrame(requestRef.current);
+        cancel(requestRef.current);
       };
   }, [progress]);
 
   const handleOnMouseEnter: MouseEventHandler<HTMLDivElement> = (event) => {
-    requestRef.current !== null && cancelAnimationFrame(requestRef.current);
     setIsPaused(true);
+    cancel(requestRef.current);
   };
 
   const handleOnMouseLeave: MouseEventHandler<HTMLDivElement> = (event) => {
-    requestRef.current = requestAnimationFrame(animate);
     previousTimeRef.current = null;
     setProgress(0);
     setIsPaused(false);
+    requestRef.current = requestAnimationFrame(animate);
   };
 
   const handleOpen = () => setIsVisible(true);
+
   const handleClose = () => setIsVisible(false);
+
+  const reset = () => {
+    previousTimeRef.current = null;
+    setProgress(0);
+    setIsPaused(false);
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  const previous = () => {
+    decrement();
+    reset();
+  };
+
+  const next = () => {
+    increment();
+    reset();
+  };
 
   return (
     <>
@@ -111,16 +144,30 @@ function Notes() {
       </div>
       {isVisible && (
         <div
-          className="max-w-md min-w-md w-full fixed top-3 right-3 flex flex-col items-center"
+          className="max-w-sm min-w-sm w-full fixed top-3 right-3 flex flex-col items-center group"
           onMouseEnter={handleOnMouseEnter}
           onMouseLeave={handleOnMouseLeave}
         >
           <div className="bg-stone-700 text-white w-full cursor-pointer">
             <div className="flex justify-between items-center p-2">
-              <span className="flex-1 flex justify-start">notes</span>
-              <span className="text-xs flex flex-1 justify-center">{`${
-                slide + 1
-              } of ${text.length}`}</span>
+              <span className="flex-1 flex justify-start">
+                {LABEL_TEXT_NOTES}
+              </span>
+              <span className="text-xs flex flex-1 justify-center gap-x-2">
+                <span
+                  className="opacity-0 group-hover:opacity-100 cursor-pointer hover:underline"
+                  onClick={previous}
+                >
+                  &larr;
+                </span>
+                {`${slide + 1} of ${text.length}`}{' '}
+                <span
+                  className="opacity-0 group-hover:opacity-100 cursor-pointer hover:underline"
+                  onClick={next}
+                >
+                  &rarr;
+                </span>
+              </span>
               <button
                 onClick={handleClose}
                 className="text-base text-white flex-1 flex justify-end"
@@ -133,7 +180,7 @@ function Notes() {
               style={{ width: `${progress}%` }}
             ></div>
           </div>
-          <div className="relative h-28 w-[452px] overflow-hidden flex justify-center">
+          <div className="relative h-28 w-[400px] overflow-hidden flex justify-center">
             <AnimatePresence initial={false}>
               <motion.div
                 key={slide}
@@ -145,7 +192,7 @@ function Notes() {
                   x: { type: 'spring', stiffness: 300, damping: 30 },
                   opacity: { duration: 0.2 },
                 }}
-                className="text-xs absolute max-w-md w-[448px] border-l border-b border-r p-2 shadow bg-white"
+                className="text-xs absolute max-w-md w-[384px] border-l border-b border-r p-2 shadow bg-white"
               >
                 {text[slide]}
               </motion.div>
