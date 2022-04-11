d979413596ae2ce5bf3b396cb96fb385b0771143
TadjhBrooks <tadjh.brooks@gmail.com>
2022-04-11 16:01:46 -0700

    feat(ui): added disabled state to submit button

--- src/components/Inputs/SubmitButton/SubmitButton.tsx
+++ src/components/Inputs/SubmitButton/SubmitButton.tsx
@@ -1,11 +1,17 @@
-import { HTMLAttributes } from 'react';
+import { ButtonHTMLAttributes } from 'react';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
 
-export interface SubmitButtonProps extends HTMLAttributes<HTMLDivElement> {
+export interface SubmitButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
   isLoading?: boolean;
 }
-function SubmitButton({ isLoading = false, children }: SubmitButtonProps) {
+function SubmitButton({
+  isLoading = false,
+  children,
+  disabled,
+}: SubmitButtonProps) {
   if (isLoading) {
     return (
       <div className="w-[72px] flex justify-center items-center">
@@ -17,7 +23,12 @@ function SubmitButton({ isLoading = false, children }: SubmitButtonProps) {
   return (
     <button
       type="submit"
-      className="text-xs hover:cursor-pointer bg-gray-700 hover:bg-gray-900 border transition-colors text-white text-opacity-90 px-4 py-1"
+      className={clsx(
+        'text-xs hover:cursor-pointer bg-gray-700 hover:bg-gray-900',
+        'border transition-colors text-white text-opacity-90 px-4 py-1',
+        'disabled:opacity-50 disabled:cursor-default hover:disabled:bg-gray-700'
+      )}
+      disabled={disabled}
     >
       {children}
     </button>
